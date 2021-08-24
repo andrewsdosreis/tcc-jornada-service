3f3ed3b69c063d98be318e2d4c6b5a36d8212987
Andrews Reis <andrewsdosreis@gmail.com>
2021-08-24 17:16:29 -0300

    Adiciona estrutura inicial do projeto

---
+++ src/main/java/br/com/lutadeclasses/jornadaservice/JornadaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lutadeclasses.jornadaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaServiceApplication.class, args);
+	}
+
+}
