730b11f29e66cb6104cfcd72856f159d35f0206b
Andrews Reis <andrewsdosreis@gmail.com>
2021-08-28 00:24:15 -0300

    Adiciona classes DTO de request

---
+++ src/main/java/br/com/lutadeclasses/jornadaservice/model/RequestNovaCartaDto.java
@@ -0,0 +1,18 @@
+package br.com.lutadeclasses.jornadaservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class RequestNovaCartaDto {
+
+    private String descricao;
+
+}
